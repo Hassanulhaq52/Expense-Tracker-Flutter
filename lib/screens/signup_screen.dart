eb5661221fdcaa04315cafc88ed62906b8816713
Hassan Ul Haq <81625175+Hassanulhaq52@users.noreply.github.com>
2023-01-06 13:00:22 +0500

    home screen created.

---
+++ lib/screens/signup_screen.dart
@@ -0,0 +1,18 @@
+import 'package:expense_tracker_flutter/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
